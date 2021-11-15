c16573f93a06d11b0cbd88f83cb5decc1e9efd8f
Nathan Tarbert <nate4t@hotmail.comm>
2021-11-15 12:21:21 -0800

    set values for bird and the jump

--- bird.js
+++ bird.js
@@ -1,13 +1,27 @@
 const birdElem = document.querySelector('[data-bird]');
 const BIRD_SPEED = 0.5;
+const JUMP_DURATION = 125;
+let timeSinceLastJump = Number.POSITIVE_INFINITY;
+
 
 export function setupBird() {
-    
+    setTop(window.innerHeight / 2); //this will place the bird in the middle of the screen
+    document.removeEventListener('keydown', handleJump);
+    document.addEventListener('keydown', handleJump);
 }
 
-export function updateBird(delta) {
-    setTop(getTop() + BIRD_SPEED * delta);
-    console.log(getTop());
+export function updateBird(delta) { //check time since last jump and update
+    if (timeSinceLastJump < JUMP_DURATION) {
+        setTop(getTop() - BIRD_SPEED * delta); //this moves the bird up
+    }else {
+        setTop(getTop() + BIRD_SPEED * delta); //this moves the bird down
+    }
+    
+    timeSinceLastJump += delta;
+}
+
+export function getBirdRect() {
+    return birdElem.getBoundingClientRect(); //this will get the position of the bird to determine if the bird has gone off the screen
 }
 
 function setTop(top) {
@@ -16,4 +30,10 @@ function setTop(top) {
 
 function getTop() {
     return parseFloat(getComputedStyle(birdElem).getPropertyValue('--bird-top'));
+}
+
+function handleJump(e) {
+    if (e.code !== 'Space') return;
+
+    timeSinceLastJump = 0;
 }
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-import { updateBird } from './bird.js';
+import { updateBird, setupBird, getBirdRect } from './bird.js';
 
 document.addEventListener("keypress", handleStart, { once: true });
 const title = document.querySelector('[data-title]');
+const subtitle = document.querySelector('[data-subtitle]');
 
 let lastTime;
 
@@ -13,15 +14,29 @@ function updateLoop(time) {
     }
     const delta = time - lastTime;
     updateBird(delta);
+    if (checkLose()) return handleLose(); //this will end the game if checkLose is true
     lastTime = time;
     window.requestAnimationFrame(updateLoop);
 }
 
+function checkLose() {
+    const birdRect = getBirdRect();
+    const outsideworld = birdRect.top < 0 || birdRect.bottom > window.innerHeight; //this will check to see if the bird hits the top or the bottom
+    return outsideworld;
+}
+
 function handleStart() {
     title.classList.add('hide');
+    setupBird();
+    lastTime = null; //resetting the lastTime so if the game is restarted it doesn't carry over previous values
     window.requestAnimationFrame(updateLoop);
 }
 
 function handleLose() {
-
+    setTimeout(() => {
+        title.classList.remove('hide');
+    subtitle.classList.remove('hide');
+    subtitle.textContent = '0 pipes';
+    document.addEventListener("keypress", handleStart, { once: true }); //this will restart the game by "keypress" once the game ends
+    }, 100);//adds a delay before the game can be restarted    
 }   
